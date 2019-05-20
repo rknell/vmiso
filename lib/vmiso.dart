9f3eec764a21f8bd3729f990da48e3a6b5642b53
Ryan Knell <rknell@snappyapps.com.au>
2019-05-21 08:25:24 +1000

    Removing outdated imports

--- lib/vmiso.dart
+++ lib/vmiso.dart
@@ -1,9 +1,7 @@
 library vmiso;
 
-import 'InheritedStore.dart';
 import 'observer.dart';
 import 'view_model.dart';
 
-export 'InheritedStore.dart';
 export 'observer.dart';
 export 'view_model.dart';
